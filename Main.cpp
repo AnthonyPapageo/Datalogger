cc68eeb744a7d4ee82dc6442ff586359c2f4992c
AnthonyPapageo <37082882+Cypher315@users.noreply.github.com>
2019-06-06 13:30:17 +0200

    updated include

--- Main.cpp
+++ Main.cpp
@@ -5,9 +5,8 @@
 
 //////////////AVR//////////////
 #include <avr/io.h>
-#include <avr/interrupt.h>
 #include <avr/wdt.h>
-#include <util/delay.h>
+#include <avr/interrupt.h>
 
 //////////////ARDUINO/////////////
 #include <LiquidCrystal.h>
@@ -15,42 +14,33 @@
 #include <LiquidMenu.h>
 #include <Pushbutton.h>
 #include <INA.h>
+#include <RTClib.h>
 #include <Wire.h>
 
 
+
+
 //////////////CUSTOM//////////////
-#include "C:/Users/Anthony/Documents/BA3 Electronique/Stages/Datalogger/Code/Library/Custom/utility.h"
-#include "C:/Users/Anthony/Documents/BA3 Electronique/Stages/Datalogger/Code/Library/Custom/MAX31856.h"
-#include "C:/Users/Anthony/Documents/BA3 Electronique/Stages/Datalogger/Code/Library/Custom/global.h"
-#include "C:/Users/Anthony/Documents/BA3 Electronique/Stages/Datalogger/Code/Library/Custom/menu.h"
-#include "C:/Users/Anthony/Documents/BA3 Electronique/Stages/Datalogger/Code/Library/Custom/measure.h"
+#include "utility.h"
+#include "MAX31856.h"
+#include "global.h"
+#include "menu.h"
+#include "measure.h"
 
 
 void wdt_init(void) __attribute__((naked)) __attribute__((section(".init3")));
 
-void enableIntButton(void);
-void buttonsCheck(void);
-
-
 
 ///////OBJECT INSTANTATION///////
-LiquidCrystal lcd(RS, RW, ENABLE_LCD, D4, D5, D6, D7);//4bit
-INA_Class INA;
-LiquidMenu menu(lcd);
+volatile uint16_t counterInterrupt = 0;
 
-/*Pushbutton butBack(40,PULL_UP_DISABLED); //Extern RC debounce+ pull up so no need of internal pull up
+Pushbutton butBack(40, PULL_UP_DISABLED); //Extern RC debounce + pull up so no need of internal pull up
 Pushbutton butLeft(41, PULL_UP_DISABLED);
 Pushbutton butDown(37, PULL_UP_DISABLED);
 Pushbutton butUp(36, PULL_UP_DISABLED);
 Pushbutton butRight(35, PULL_UP_DISABLED);
-Pushbutton butOk(34, PULL_UP_DISABLED);*/
+Pushbutton butOk(34, PULL_UP_DISABLED);
 
-volatile bool bOk = false;
-volatile bool bUp = false;
-volatile bool bRight = false;
-volatile bool bLeft = false;
-volatile bool bDown = false;
-volatile uint32_t counterInterrupt = 0;
 
 ISR(TIMER1_COMPA_vect)
 {
@@ -60,58 +50,25 @@ ISR(TIMER1_COMPA_vect)
 		counterInterrupt = 0;
 	}
 	counterInterrupt++;
-	PORTA ^=(1<<3);
+	PORTJ ^=(1<<5);
 }
-
-ISR(INT2_vect) //OK button
-{
-	bOk =  true;
-	_delay_us(2000);
-}
-
-ISR(INT4_vect) //Up button
-{
-	bUp = true;
-	_delay_us(20);
-}
-
-ISR(INT5_vect) //Right button
-{
-	bRight = true;
-	_delay_us(20);
-}
-
-ISR(INT6_vect) //Down button
-{
-	bDown = true;
-	_delay_us(20);
-}
-ISR(INT7_vect) //Left button
-{
-	bLeft = true;
-	_delay_us(20);
-}
-
-
 
 
 void setup()
 {
 	Wire.begin();
-	enableIntButton();
-	
+	Wire.setClock(INA_I2C_STANDARD_MODE);
 	enableVref(false); //disable LT1461
 	initRTC(); //set RTC time of compilation if it has lost power
 	menu.init();
-	//initMux(); //Set mux pins as output and enable multiplexer
-	
+	initMux(); //Set mux pins as output and enable multiplexer
 	
 	lcd.begin(LCD_COL, LCD_ROWS);
-	lcd.createChar(GLYPH::upArrowIndex, GLYPH::upArrow);
+	lcd.createChar(GLYPH::upArrowIndex, GLYPH::upArrow); //Add arrow glyphs
 	lcd.createChar(GLYPH::downArrowIndex,GLYPH::downArrow);
 	lcd.createChar(GLYPH::rightArrowIndex, GLYPH::rightArrow);
 	lcd.createChar(GLYPH::leftArrowIndex, GLYPH::leftArrow);
-
+	
 	AddVariableToLine();
 	attachFunctionToLine();
 	enableScrolling();
@@ -122,16 +79,21 @@ void setup()
 	menu.set_focusPosition(Position::LEFT);
 	menu.set_focusSymbol(Position::LEFT, GLYPH::rightArrow);
 	
-	LoadFromEEPROM(); //Load last config
-	
+
+	LoadFromEEPROM(); //We do it here because we are sure that the power supply is already in a stable state
 	menu.update();
 	menu.switch_focus(true);
+	DDRJ |= 0x60; //LED 2 and LED 3 as output
+	DDRC &= 0xF0; //but as input
+	DDRG &= 0xFC; //but back et left as input
+	DDRL |= (1<<6);
+	PORTL |= (1<<6); //CS SD high
+	SPI.beginTransaction(SPISettings(400000,MSBFIRST,SPI_MODE0));
+	setMux(CS_DISABLE);
 	
-	DDRA = 0xFF;
-	
-	while(1) 
+	while(1)
 	{
-		buttonsCheck();
+		buttonsCheck(butBack, butLeft, butDown, butUp, butRight, butOk);
 		if((counterInterrupt == IntervalMeasure) && (current_screen == &Measuring_Screen)) //Interval measures is the time interval we need in seconds
 		{
 			getAllMeasures();
@@ -143,77 +105,21 @@ void setup()
 		{			
 			endMeasures();
 		}
-		PORTA ^= (1<<0);
 	}
 }
 
+	
+	
+	
 	
 void loop(){} //already have while(1) in setup 
-	
-void wdt_init(void)
-{
-	MCUSR = 0;
-	wdt_disable();
-	return;
-}
 
-void enableIntButton(void)
+void wdt_init(void) //Disable watchdog after soft reset
 {
-	DDRD &= ~(1 << 2); //INT2 as input
-	DDRE &= 0x0F; //INT 7:4 as input
-
-	cli(); //Disable Global Interrupt
-	EIMSK = 0; //Disable all external interrupt
-	EICRA |= (1 << 5); //Falling edge on INT2
-	EICRA &= ~(1 << 4);
-	EICRB = 0xAA; //0b1010 1010 Falling edge INT7:4
-	EIMSK |= 0xF4; //0b1111 0100 Enable external interrupt
-	sei(); //Enable Global Interrupt
+    MCUSR = 0;
+    wdt_disable();
+    return;
 }
 
 
-void buttonsCheck(void)
-{
-	current_screen = menu.get_currentScreen();
-	
-	if(bUp)
-	{
-		bUp = false;
-		menu.switch_focus(false); //Go up
-		return;
-	}
-	else if(bDown)
-	{
-		bDown = false;
-		menu.switch_focus(true); //Go down
-		return;
-	}
-	else if(bRight)
-	{
-		bRight = false;
-		menu.call_function(7); //Increment function saved on the seventh
-		return;
-	}
-	else if(bLeft)
-	{
-		bLeft = false;
-		menu.call_function(6); //Decrement function saved on the sixth
-		return;
-	}
-	else if(bOk)
-	{
-		bOk=false;
-		uint8_t i = 1;
-		while((menu.call_function(i)) && (i<= 5) && (menu.get_currentScreen() == current_screen))
-		//Call the function and check what it returns
-		//if the call_function returns false, there is no more function
-		//so we get out. Verify that we don't go over 5 (reserved functions)
-		//if we changed screen, it's we don't need to call an other function 
-		{
-			i++;
-		}
-		return;
-	}
-	_delay_ms(100);
-}
 
